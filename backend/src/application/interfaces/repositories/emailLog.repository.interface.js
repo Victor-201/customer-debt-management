8a1b4d1728e8c268e3f8807fdcdc4a6c33fefbfb
Victor-201 <4.victor.201@gmail.com>
2026-01-13 02:28:17 +0700

    refactor(interfaces): update repository and service interfaces

---
+++ backend/src/application/interfaces/repositories/emailLog.repository.interface.js
@@ -0,0 +1,9 @@
+export default class EmailLogRepository {
+  hasSent(invoiceId, emailType) {
+    throw new Error("METHOD_NOT_IMPLEMENTED");
+  }
+
+  save(emailLog) {
+    throw new Error("METHOD_NOT_IMPLEMENTED");
+  }
+}
