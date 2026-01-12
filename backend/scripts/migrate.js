eacec41bed6fa779bf84ca49af5f2177f660cbf4
bedive <trungbed1ve@gmail.com>
2026-01-12 10:53:46 +0700

    fix(script): migrate scripts from cjs to esm

--- backend/scripts/migrate.js
+++ backend/scripts/migrate.js
@@ -1,7 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const { Pool } = require('pg');
-require('dotenv').config();
+import fs from 'fs';
+import path from 'path';
+import { Pool } from 'pg';
+import "dotenv/config";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pool = new Pool({
   host: process.env.DB_HOST,
